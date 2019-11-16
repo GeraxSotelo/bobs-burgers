b22610427fc765d4df93e292613087d36bf14f69
Gerax Sotelo <gsotelo23@gmail.com>
2019-11-16 14:47:03 -0700

    Add multiplier

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-let money = 0;
+let clickUpgrades = {
+  spatulas: {
+    name: ["Big", "Bigger"],
+    price: [5, 10],
+    multiplier: [1, 3]
+  },
+}
+
 let moneyDisplay = document.querySelector("#money-display");
 let upgradeSpatula = document.querySelector("#upgrade-spatula");
 let spatulaDisplay = document.querySelector("#spatula-display");
 let spatulaPrice = document.querySelector("#spatula-price");
-
-let clickUpgrades = {
-  spatula: {
-    name: ["Big", "Bigger"],
-    price: [5, 10],
-    multiplier: [2, 4]
-  },
-}
+let spatulasIndex = 0;
+let sPrice = clickUpgrades.spatulas.price;
+let sName = clickUpgrades.spatulas.name;
+let sMultiplier = 0;
+let eMultiplier = 0;
+let money = 0;
 
 // document.querySelector("#multiplier-img-1").style.display = "unset";
 
@@ -19,24 +24,24 @@ function update() {
 }
 
 function flip() {
-  money++;
+  money += 1 + sMultiplier + eMultiplier;
   update();
 }
 
-let spatulaIndex = 0;
-let sPrice = clickUpgrades.spatula.price;
-let sName = clickUpgrades.spatula.name;
+
 function buySpatulaUpgrade() {
-  if (money >= sPrice[spatulaIndex]) {
-    money -= sPrice[spatulaIndex];
-    if (spatulaIndex < sPrice.length) {
-      spatulaDisplay.innerText = sName[spatulaIndex + 1];
-      spatulaPrice.innerText = sPrice[spatulaIndex + 1];
-    } else {
+  if (spatulasIndex < sPrice.length && money >= sPrice[spatulasIndex]) {
+    money -= sPrice[spatulasIndex];
+    sMultiplier = clickUpgrades.spatulas.multiplier[spatulasIndex];
+    spatulasIndex++;
+    if (spatulasIndex >= sPrice.length) {
       spatulaDisplay.innerText = "";
       spatulaPrice.innerText = "";
+    } else {
+      spatulaDisplay.innerText = sName[spatulasIndex];
+      spatulaPrice.innerText = `$${sPrice[spatulasIndex]}`;
     }
-    update();
-    spatulaIndex++;
   }
+
+  update();
 }
